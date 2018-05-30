86cae8c05a4cad32f38ce5857741d2d794d97dec
medegor44 <medegor44@gmail.com>
2018-05-30 20:46:52 +0300

    Improved work witch Codeforces API

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import json
 from problem import Problem
 
 
+lang = 'ru'
+
+
 def html_print(problems, file):
     file.write("<!DOCTYPE html>")
     file.write("<html>\n")
@@ -17,7 +20,7 @@ def html_print(problems, file):
         file.write("    <tr>\n")
 
         file.write("      <td>\n")
-        file.write("<a href={0}>{1}</a>\n".format(p.get_url(), p.name))
+        file.write("        <a href={0}?locale={1}>{2}</a>\n".format(p.get_url(), lang, p.name))
         file.write("      </td>\n")
 
         file.write("    </tr>\n")
@@ -35,12 +38,12 @@ for i in range(len(t)):
 tags = set(t)
 
 try:
-    response = rq.get('http://codeforces.com/api/problemset.problems?locale=ru').json()
+    api_request = 'http://codeforces.com/api/problemset.problems?lang={0}&tags={1}'.format(lang, ';'.join(tags))
+    response = rq.get(api_request).json()
 
     problems = []
     for kv, stat in zip(response['result']['problems'], response['result']['problemStatistics']):
-        if tags.issubset(kv['tags']):
-            problems.append(Problem(kv['contestId'], kv['index'], kv['name'], kv['tags'], stat['solvedCount']))
+        problems.append(Problem(kv['contestId'], kv['index'], kv['name'], kv['tags'], stat['solvedCount']))
 
     problems.sort(key=lambda x: x.solved_cnt, reverse=True)
 
