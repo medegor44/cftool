eb7a40c2e212e827210c08f67064576df4578980
medegor44 <medegor44@gmail.com>
2018-05-30 21:48:06 +0300

    Now it grabs user's submissions and highlits it

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests as rq
 import sys
+import webbrowser
 import json
-from problem import Problem
+import os
+from problem import Problem, Verdict
 
 
-lang = 'ru'
-
-
+# prints list of problems into simple html table
 def html_print(problems, file):
     file.write("<!DOCTYPE html>")
     file.write("<html>\n")
@@ -17,9 +17,15 @@ def html_print(problems, file):
     file.write("  <table border='1'>\n")
 
     for p in problems:
+        color = 'white'
+        if p.verdict == Verdict.AC:
+            color = '#ADFF2F'
+        elif p.verdict == Verdict.FAIL:
+            color = '#FA8072'
+
         file.write("    <tr>\n")
 
-        file.write("      <td>\n")
+        file.write("      <td bgcolor={0}>\n".format(color))
         file.write("        <a href={0}?locale={1}>{2}</a>\n".format(p.get_url(), lang, p.name))
         file.write("      </td>\n")
 
@@ -29,25 +35,71 @@ def html_print(problems, file):
 
     file.write("</html>")
 
+    file.close()
 
-t = sys.argv[1:]
 
-for i in range(len(t)):
-    t[i] = t[i].replace('_', ' ')
-
-tags = set(t)
-
-try:
-    api_request = 'http://codeforces.com/api/problemset.problems?lang={0}&tags={1}'.format(lang, ';'.join(tags))
-    response = rq.get(api_request).json()
+# gets problems with some tags from Codeforces
+def get_problems(lang, tags):
+    api_problems_request = "http://codeforces.com/api/problemset.problems?lang={0}&tags={1}".format(lang,
+                                                                                                    ';'.join(tags))
+    problems_response = rq.get(api_problems_request).json()
 
     problems = []
-    for kv, stat in zip(response['result']['problems'], response['result']['problemStatistics']):
+    for kv, stat in zip(problems_response['result']['problems'], problems_response['result']['problemStatistics']):
         problems.append(Problem(kv['contestId'], kv['index'], kv['name'], kv['tags'], stat['solvedCount']))
+
+    return problems
+
+
+# checks which problems were solved
+def check_solved(lang, user, problems):
+    verdicts = {}
+    for p in problems:
+        verdicts[p.get_id()] = p.verdict
+
+    api_user_stat_request = "http://codeforces.com/api/user.status?lang={0}&handle={1}&from=1".format(lang, user)
+    response = rq.get(api_user_stat_request).json()
+
+    for submission in response['result']:
+        if not 'contestId' in submission['problem']:
+            continue
+
+        id = str(submission['problem']['contestId']) + submission['problem']['index']
+        verdict = submission['verdict']
+
+        verd = Verdict.FAIL
+
+        if verdict == "OK":
+            verd = Verdict.AC
+
+        if id in verdicts:
+            old_verdict = verdicts[id]
+            verdicts[id] = verd if old_verdict != Verdict.AC else old_verdict
+
+    for problem in problems:
+        id = problem.get_id()
+        if id in verdicts:
+            problem.verdict = verdicts[id]
+
+
+try:
+    user = sys.argv[1]
+    lang = 'ru'
+    t = sys.argv[2:]
+
+    for i in range(len(t)):
+        t[i] = t[i].replace('_', ' ')
+
+    tags = set(t)
+
+    problems = get_problems(lang, tags)
+    check_solved(lang, user, problems)
 
     problems.sort(key=lambda x: x.solved_cnt, reverse=True)
 
     html_print(problems, open("index.html", "w"))
+    webbrowser.open("file://" + os.path.realpath("index.html"))
+
 except IOError:
     print("Can't connect to Codeforces. Wait a bit or check your Internet connection.")
 except json.JSONDecodeError:
--- problem.py
+++ problem.py
@@ -1,16 +1,37 @@
+from enum import Enum
+
+
+class Verdict(Enum):
+    NA = -1
+    FAIL = 0
+    AC = 1
+
+
 class Problem:
-    def __init__(self, contestID, index, name, tags, cnt):
+    def __init__(self, contestID, index, name, tags, cnt, verdict=Verdict.NA):
         self._name = name
         self._solved_cnt = cnt
         self._tags = tags
         self._contestID = contestID
         self._index = index
+        self._verdict = verdict
+
+    def get_id(self):
+        return str(self.contestID) + self.index
 
     def get_url(self):
         return "http://codeforces.com/problemset/problem/{0}/{1}".format(self._contestID, self._index)
 
     def __str__(self):
         return "name = {0}, solved = {1} tags = {2}".format(self._name, self._solved_cnt, self._tags)
+
+    @property
+    def verdict(self):
+        return self._verdict
+
+    @verdict.setter
+    def verdict(self, v):
+        self._verdict = v
 
     @property
     def contestID(self):
